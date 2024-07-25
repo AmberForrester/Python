eb25eb9e3863876e72cfba732196dd2dc9508cd6
Amber Forrester <DivineIntelligence3.0@gmail.com>
2024-07-25 17:15:04 -0400

    Python types example + find the remainder - done

--- day4/p2_numbers.py
+++ day4/p2_numbers.py
@@ -1,11 +1,11 @@
 # P2: Declare two numbers variables and find the remainder - 
 
-# Define two numbers
+# Define two numbers and assign values to `number1` and `number2`:
 number1 = 5
 number2 = 7
 
-# Add the numbers using the + operator 
+# Add the numbers using the + operator, storing the result in the variable called `result`: 
 result = number1 + number2
 
-# Print the result
+# Print the result using the `print()` function: 
 print("The sum of", number1, "and", number2, "is:", result)
---
+++ day4/types.py
@@ -0,0 +1,12 @@
+a = 10.6
+myType = type(a)
+print(myType)
+
+# Output = <class 'float'>
+
+
+a = "Sadeed"
+myType = type(a)
+print(myType)
+
+# Output = <class 'float'>
