80d459285255cec99324fc0fcd271004a6d0d160
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-08-14 12:02:47 -0400

    To Do List

--- day17/todolist/app.py
+++ day17/todolist/app.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy # ORM - Object Relational Mapper
+from datetime import datetime
 
+
+
+
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todolist.db"
+# initialize the app with the extension
+db = SQLAlchemy(app) # Create an object db to initialize the object.
+
+
+class Todolist(db.Model):
+    taskid = db.Column(db.Integer, primary_key = True) # Only one primary key. 
+    task = db.Column(db.String(250), nullable = True)
+    desc = db.Column(db.String(500), nullable = True)
+    creation_date = db.Column(db.DateTime, default=datetime.utcnow)
+    
+def __repr__(self) -> str:
+    return f'{self.taskid} - {self.task}'
+
+
+
 
 @app.route("/") # Default route 
 def index():
