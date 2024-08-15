a6de0fbb870aaa0e9e18b5efc05c80416728bea0
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-08-15 11:58:21 -0400

    db connection made w/front end

--- day17/todolist/app.py
+++ day17/todolist/app.py
@@ -2,17 +2,15 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy # ORM - Object Relational Mapper
 from datetime import datetime
 
-
-
-# create the app
+# Create the app:
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
+
+# Configure the SQLite database, relative to the app instance folder:
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todolist.db"
-# initialize the app with the extension
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the app with the extension:
 db = SQLAlchemy(app) # Create an object db to initialize the object.
-
-
-
 class Todolist(db.Model):
     taskid = db.Column(db.Integer, primary_key = True) # Only one primary key. 
     task = db.Column(db.String(250), nullable = True)
@@ -20,27 +18,30 @@ class Todolist(db.Model):
     creation_date = db.Column(db.DateTime, default=datetime.now())
     
     def __repr__(self) -> str:
-        return f'{self.taskid} - {self.task}'
+        return f'{self.taskid} - {self.task} - {self.desc}'
 
-with app.app_context():
-    db.create_all()
 
 
 
 @app.route("/") # Default route 
 def index():
-    return render_template('index.html')
+    todo = Todolist(task='reading a book', desc='Dotcom Secrets')
+    db.session.add(todo)
+    db.session.commit()
+    myalltodolist = Todolist.query.all() # Connect to variable to throw back 
+    return render_template('index.html', myalltodolist=myalltodolist)
 
-# @app.route("/blog")
-# def blog():
-#     return "<p>This is my Blog</p>"
+@app.route("/getlist") # Default route 
+def getlists():
+    myalltodolist = Todolist.query.all() # Connect to variable to throw back on home page.
+    print(myalltodolist)
+    return '<p>Get All the TO DO</p>'
 
-
-# @app.route("/contact")
-# def contact():
-#     return "<p>This is my Contact Page</p>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True) # Development mode not production to show errors 
+    # Create db tables in an application context BEFORE creating the db:
+    with app.app_context():
+        db.create_all() # Seeding the data.
+    app.run(debug=True) # Development mode not production to show errors. 
     
