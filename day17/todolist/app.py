f79fd87b441762aa0c5044cc79c50cae52ee004d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-08-19 11:00:30 -0400

    search functionality applied

--- day17/todolist/app.py
+++ day17/todolist/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy # ORM - Object Relational Mapper
 from datetime import datetime
 
@@ -15,7 +15,7 @@ class Todolist(db.Model):
     taskid = db.Column(db.Integer, primary_key = True) # Only one primary key. 
     task = db.Column(db.String(250), nullable = True)
     desc = db.Column(db.String(500), nullable = True)
-    creation_date = db.Column(db.DateTime, default=datetime.now().replace(second=0, microsecond=0))
+    creation_date = db.Column(db.DateTime, default=lambda: datetime.now().replace(second=0, microsecond=0))
     
     def __repr__(self) -> str:
         return f'{self.taskid} - {self.task} - {self.desc}'
@@ -39,6 +39,9 @@ def index():
             db.session.add(todo)
             db.session.commit()
             
+            # Redirect to the GET route to avoid resbmission on refresh: 
+            return redirect('/')
+            
     # Query all to-do items to display them:
     myalltodolist = Todolist.query.all() # Connect to variable to throw back
      
@@ -51,6 +54,53 @@ def getlists():
     myalltodolist = Todolist.query.all() # Connect to variable to throw back on home page.
     print(myalltodolist)
     return '<p>Get All the TO DO</p>'
+
+@app.route("/delete/<int:taskid>") # Default route for delete when button is pressed:
+def delete(taskid): # Pass the specific task to delete R/T id
+    mytodo = Todolist.query.filter_by(taskid=taskid).first() # Pick up the first taskid
+    db.session.delete(mytodo) # Pass the filtered taskid for deletion 
+    db.session.commit()
+    return redirect('/')
+
+@app.route("/update/<int:taskid>", methods=['GET', 'POST']) # Default route for delete when button is pressed:
+def update(taskid): # Pass the specific task to delete R/T id
+    
+    # Manage the state for updating:
+    if request.method == 'POST':
+        mytask = request.form['task']
+        mydesc = request.form['desc']
+        # Update page is now ready to call mytodo for update:
+        mytodo = Todolist.query.filter_by(taskid=taskid).first() # Pick up the first taskid
+        if mytodo:
+        # Populate the values that are set from original input of task and description
+            mytodo.task = mytask
+            mytodo.desc = mydesc
+            db.session.add(mytodo)
+            db.session.commit()
+            return redirect('/')
+        
+    mytodo = Todolist.query.filter_by(taskid=taskid).first()
+    
+    return render_template('update.html', mytodo=mytodo)
+
+@app.route("/search", methods=['GET'])
+def search(): 
+    
+    # Looking for a single search term: 
+    query = request.args.get('query', " ")
+    if query:
+        
+        # SQLAlchemy's filer method to search for task or desc matching the search query:
+        # Filter to do list items based on the search query:
+        # ilike is used for case-insensitive matching: 
+        SearchResults = Todolist.query.filter(
+            (Todolist.task.ilike(f'%{query}')) |    # Search query checks both with OR |
+            (Todolist.desc.ilike(f'%{query}'))
+        ).all()
+    else:
+        SearchResults = []
+        
+    return render_template('search.html', SearchResults=SearchResults, query=query)
 
 
 
